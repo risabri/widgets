f747f067028bb63e1a5da2f69049bf28606fc1b9
prifina-admin <tero@prifina.com>
2021-03-04 17:21:00 +0200

    hello props

--- packages/hello/src/index.js
+++ packages/hello/src/index.js
@@ -1,6 +1,8 @@
 /**
  * Entrypoint of the Remote Component.
  */
-import { App } from "./App";
+//import { App } from "./App";
 
-export default App;
+//export default App;
+import Hello from "./Hello";
+export default Hello;
