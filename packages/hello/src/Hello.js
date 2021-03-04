b2810a121cd533ff67ff4174e8f3657a9e89a3ed
prifina-admin <tero@prifina.com>
2021-03-04 21:28:55 +0200

    hello props

--- packages/hello/src/Hello.js
+++ packages/hello/src/Hello.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { usePrifina } from "@prifina/hooks";
+import { usePrifina, useHooks } from "@prifina/hooks";
 
 const Container = styled.div`
   height: 200px;
@@ -14,18 +14,19 @@ const Hello = (props) => {
   console.log("HELLO PROPS ", props);
   const { msg, data } = props;
 
-  const { currentUser, onUpdate, check } = usePrifina({ appID: "HelloWidget" });
-  console.log("Logged in user ", currentUser);
-
-  const msgText = msg || "Hello, " + currentUser.name;
+  //const { currentUser, onUpdate, check } = usePrifina({ appID: "HelloWidget" });
+  const prifina = useHooks();
+  //console.log("Logged in user ", currentUser);
+  const msgText = msg || "Hello, ";
+  //const msgText = msg || "Hello, " + currentUser.name;
   const [helloText, setHelloText] = useState(msgText);
 
   onUpdate((data) => {
     console.log("HELLO HERE ", data);
   });
 
-  console.log(onUpdate, typeof onUpdate);
-  console.log(check());
+  //console.log(onUpdate, typeof onUpdate);
+  //console.log(check());
   useEffect(() => {
     console.log("UPDATE DATA ", data);
     if (typeof data !== "undefined") {
