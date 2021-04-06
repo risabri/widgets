5fe9e221544c137f501ade4e71f0907f04b796f8
prifina-admin <tero@prifina.com>
2021-04-06 10:00:18 +0300

    watch

--- packages/watch/config/remote-component.config.js
+++ packages/watch/config/remote-component.config.js
@@ -7,6 +7,7 @@ module.exports = {
   resolve: {
     react: require("react"),
     "styled-components": require("styled-components"),
+    "@prifina/hooks": require("@prifina/hooks"),
     moment: require("moment"),
     "moment-timezone": require("moment-timezone"),
   },
