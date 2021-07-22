5cb61a310c9bca93d2f18ff63f4c3747b6a1d46f
hamza <hamzahalilovic.12@gmail.com>
2021-07-22 03:31:54 +0200

    weather widget update

--- packages/weather/src/App.js
+++ packages/weather/src/App.js
@@ -4,6 +4,28 @@ import useFetch from "./hooks/useFetch";
 import { usePrifina } from "@prifina/hooks";
 import { API_KEY, API_BASE_URL } from "./apis/config";
 
+import {
+  Flex,
+  ChakraProvider,
+  Text,
+  Input,
+  Image,
+  Icon,
+} from "@chakra-ui/react";
+
+import { LocationIcon } from "./assets/icons";
+
+const containerStyle = {
+  width: "308px",
+  height: "296px",
+  background: "linear-gradient(180deg, #D2DEEA 0%, #00416A 100%)",
+  borderRadius: "30px",
+  boxShadow: "0px 2px 8px rgba(91, 92, 91, 0.2)",
+  paddingTop: 31,
+  flexDirection: "column",
+  justifyContent: "space-between",
+};
+
 // unique appID for the widget....
 const appID = "weatherWidget";
 
@@ -52,8 +74,12 @@ const App = (props) => {
     onUpdate(appID, dataUpdate);
   }, []);
 
+  // const { weatherData, error, isLoading, setUrl } = useFetch(
+  //   `${API_BASE_URL}/data/2.5/weather?q=${searchCity}&units=metric&appid=${API_KEY}`
+  // );
+
   const { weatherData, error, isLoading, setUrl } = useFetch(
-    `${API_BASE_URL}/data/2.5/weather?q=${searchCity}&units=metric&appid=${API_KEY}`
+    `http://api.weatherapi.com/v1/forecast.json?key=e72f4e2b049a4ca7918223846212007&q=New York&days=3&aqi=no&alerts=no`
   );
   //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
@@ -61,80 +87,226 @@ const App = (props) => {
     if (error) return <h2>Error when fetching: {error}</h2>;
     if (!weatherData && isLoading) return <h2>LOADING...</h2>;
     if (!weatherData) return null;
-    console.log(JSON.stringify(weatherData));
-    /*
-    {"coord":{"lon":24.9355,"lat":60.1695},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds",
-    "icon":"03d"}],"base":"stations","main":{"temp":-5.81,"feels_like":-11.21,"temp_min":-6.11,
-    "temp_max":-4.44,"pressure":1018,"humidity":86},"visibility":10000,"wind":{"speed":3.6,"deg":150},
-    "clouds":{"all":40},"dt":1613732443,"sys":{"type":1,"id":1332,"country":"FI","sunrise":1613713683,
-    "sunset":1613748027},"timezone":7200,"id":658225,"name":"Helsinki","cod":200}
-    */
-    //console.log(weatherData.weather);
-    const icon = weatherData.weather[0].icon;
-    const weatherCity = weatherData.name;
+
+    console.log("WEATHER DATA", weatherData);
+
+    const currentTemperature = weatherData.current.temp_c;
+    const currentCondition = weatherData.current.condition.text;
+    const currentIcon = weatherData.current.condition.icon;
+
+    const location = weatherData.location.name;
+    const locationTime = weatherData.location.localtime;
+
+    var time = new Date(locationTime);
+    const currentTime = time.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+
     return (
-      <React.Fragment>
-        <div
-          style={{
-            width: "200px",
-            height: "200px",
-            padding: "5px",
-          }}
+      <Flex justifyContent="center">
+        <Flex paddingRight="17px">
+          <Image src={currentIcon} boxSize="100px" />
+        </Flex>
+        <Flex
+          flexDirection="column"
+          alignItems="flex-start"
+          justifyContent="center"
         >
-          <div style={{ textAlign: "center", textTransform: "capitalize" }}>
-            {weatherCity}
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100px",
-            }}
-          >
-            <img
-              width={"100"}
-              src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-            />
-            <div style={{ width: "90px" }}>
-              {Math.ceil(weatherData.main.temp)}
-              {"°C"}
-            </div>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div
-              style={{
-                width: "100px",
-                textAlign: "center",
-                textTransform: "capitalize",
-              }}
+          <Flex alt="location" justifyContent="center">
+            <LocationIcon />
+            <Text fontSize="14px" fontWeight="400" color="white">
+              {location}
+            </Text>
+          </Flex>
+          <Flex alt="location" alignItems="baseline">
+            <Text
+              paddingRight="3px"
+              fontWeight="600"
+              fontSize="24px"
+              color="white"
             >
-              {" "}
-              {weatherData.weather[0].description}
-            </div>
-            <div style={{ width: "90px" }} />
-          </div>
-        </div>
-      </React.Fragment>
+              {currentTemperature}°
+            </Text>
+            <Text fontWeight="600" fontSize="16px" color="white">
+              {currentCondition}
+            </Text>
+          </Flex>
+          <Text fontSize="14px" fontWeight="400" color="white">
+            {currentTime}
+          </Text>
+        </Flex>
+      </Flex>
+    );
+  };
+
+  const bottomContainer = () => {
+    if (error) return <h2>Error when fetching: {error}</h2>;
+    if (!weatherData && isLoading) return <h2>LOADING...</h2>;
+    if (!weatherData) return null;
+
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const threeDaysData = weatherData.forecast.forecastday;
+
+    const day2 = threeDaysData[1].date;
+    const day3 = threeDaysData[2].date;
+
+    const newDate2 = new Date(day2);
+    const newDate3 = new Date(day3);
+
+    const dayDisplay2 = days[newDate2.getDay()];
+    const dayDisplay3 = days[newDate3.getDay()];
+
+    const date2 = new Date(day2);
+    const date3 = new Date(day3);
+
+    const dateNumber2 = date2.getDate();
+    const month2 = months[date2.getMonth()];
+    const dateNumber3 = date3.getDate();
+    const month3 = months[date3.getMonth()];
+
+    console.log("Three day DATA", threeDaysData);
+
+    const icon2 = threeDaysData[1].day.condition.icon;
+    const icon3 = threeDaysData[2].day.condition.icon;
+
+    const day2Min = threeDaysData[1].day.mintemp_c;
+    const day2Max = threeDaysData[1].day.maxtemp_c;
+    const day3Min = threeDaysData[2].day.mintemp_c;
+    const day3Max = threeDaysData[2].day.maxtemp_c;
+
+    return (
+      <Flex
+        paddingLeft="25px"
+        paddingRight="29px"
+        marginBottom="28px"
+        justifyContent="space-between"
+      >
+        <Flex flexDirection="column">
+          <Text fontSize="14px" fontWeight="600" color="white">
+            Tomorrow, {dateNumber2} {month2}
+          </Text>
+          <Text fontSize="14px" fontWeight="600" color="white">
+            {dayDisplay3}, {dateNumber3} {month3}
+          </Text>
+        </Flex>
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          flexDirection="column"
+        >
+          <Image src={icon3} boxSize="22px" />
+          <Image src={icon2} boxSize="22px" />
+        </Flex>
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          flexDirection="column"
+        >
+          <Text fontSize="14px" fontWeight="600" color="white">
+            {Math.round(day2Min)}°/ {Math.round(day2Max)}°
+            {/* {day2Min}°/ {day2Max}° */}
+          </Text>
+          <Text fontSize="14px" fontWeight="600" color="white">
+            {Math.round(day3Min)}°/ {Math.round(day3Max)}°
+            {/* {day3Min}°/ {day3Max}° */}
+          </Text>
+        </Flex>
+      </Flex>
+    );
+  };
+
+  const getForecast = () => {
+    if (error) return <h2>Error when fetching: {error}</h2>;
+    if (!weatherData && isLoading) return <h2>LOADING...</h2>;
+    if (!weatherData) return null;
+
+    console.log("WEATHER DATA", weatherData);
+
+    const hourData = weatherData.forecast.forecastday[0].hour;
+
+    const sixHourData = hourData.slice(0, 6);
+
+    console.log("HOUR DATA", hourData);
+
+    const hourTime = sixHourData.time;
+
+    console.log("HOUR time", hourTime);
+
+    var time = new Date(hourTime);
+    const currentTime = time.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+
+    return (
+      <Flex
+        paddingLeft="35px"
+        paddingRight="35px"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        {sixHourData.map(function (item, i) {
+          var time = new Date(item.time);
+          const currentTime = time.toLocaleString("en-US", {
+            hour: "numeric",
+            // minute: "numeric",
+            hour12: true,
+          });
+
+          return (
+            <Flex flexDirection="column" alignItems="center">
+              <Text fontSize="10px" fontWeight="600" color="white">
+                {currentTime}
+              </Text>
+              <Image src={item.condition.icon} boxSize="22px" />
+              <Text key={i} fontSize="12px" fontWeight="600" color="white">
+                {/* {item.temp_c} */}
+                {Math.round(item.temp_c)}
+              </Text>
+            </Flex>
+          );
+        })}
+      </Flex>
     );
   };
 
   return (
-    <div>
-      {/* 
-        <CitySelector onSearch={(city) => setUrl(`${API_BASE_URL}/data/2.5/forecast?q=${city}&cnt=5&appid=${API_KEY}`)} />
-  */}
-      {/* conditionally render  */}
-      {getContent()}
-    </div>
+    <ChakraProvider>
+      <Flex alt="container" style={containerStyle} flex={1}>
+        {getContent()}
+
+        {getForecast()}
+
+        {bottomContainer()}
+      </Flex>
+    </ChakraProvider>
   );
 };
 App.defaultProps = {
@@ -143,3 +315,12 @@ App.defaultProps = {
 App.displayName = "Weather";
 
 export default App;
+
+{
+  /* 
+        <CitySelector onSearch={(city) => setUrl(`${API_BASE_URL}/data/2.5/forecast?q=${city}&cnt=5&appid=${API_KEY}`)} />
+  */
+}
+{
+  /* conditionally render  */
+}
