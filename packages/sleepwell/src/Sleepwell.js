58e696f10f16e3a59473b32a32c34231fc930be1
hamza <hamzahalilovic.12@gmail.com>
2021-07-25 20:06:55 +0200

    sleepwell build

--- packages/sleepwell/src/Sleepwell.js
+++ packages/sleepwell/src/Sleepwell.js
@@ -1,52 +1,30 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+
 import { usePrifina } from "@prifina/hooks";
 
 import {
   Flex,
   ChakraProvider,
   Text,
-  Box,
   IconButton,
   ButtonGroup,
-  CircularProgress,
-  Button,
-  Input,
-  Stack,
 } from "@chakra-ui/react";
 
 import {
   ComposedChart,
   Line,
   Area,
-  Bar,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
-  Scatter,
-  ResponsiveContainer,
-  Label,
 } from "recharts";
 
 import { ArrowRight } from "./assets/icons";
 import { ArrowLeft } from "./assets/icons";
 
-import { data } from "./data";
-
-const tips = [
-  {
-    title: "Increase light exposure during the day",
-    text:
-      "Natural sunlight or bright light during the day helps keep your circadian rhythm healthy. This improves daytime energy, as well as nighttime sleep quality and duration",
-  },
-  {
-    title: "Take time to unwind",
-    text:
-      "Try shifting your nightly TV watching habit to an earlier time. Start by turning the TV off 15 minutes before you want to fall asleep, then increase that time to 30 minutes, 60 minutes, and longer.",
-  },
-];
+import { data, tips } from "./data";
 
 const containerStyle = {
   width: "308px",
@@ -63,14 +41,6 @@ const legendStyle = {
   left: -20,
 };
 
-const labelStyle = {
-  // transform: [{ rotate: "90deg" }],
-};
-
-const CustomizedLabel = () => {
-  return <Text>Hourasdsdaasddassas</Text>;
-};
-
 const appID = "sleepwell";
 
 const Sleepwell = () => {
@@ -80,39 +50,73 @@ const Sleepwell = () => {
 
   const [step, setStep] = useState(0);
 
-  const [displayTitle, setDisplayTitle] = useState("");
-  const [displayText, setDisplayText] = useState("");
+  console.table(tips);
 
-  function onChange() {
-    console.log("hello!");
-    waveHello();
-  }
-
-  let stepProgress = 0;
+  let title = 0;
+  let text = 0;
 
   switch (step) {
     case 0:
-      stepProgress = 25;
+      title = tips[0].title;
+      text = tips[0].text;
       break;
     case 1:
-      stepProgress = 50;
+      title = tips[1].title;
+      text = tips[1].text;
       break;
     case 2:
-      stepProgress = 75;
+      title = tips[2].title;
+      text = tips[2].text;
       break;
     case 3:
-      stepProgress = 100;
+      title = tips[3].title;
+      text = tips[3].text;
       break;
-    case 3:
-      stepProgress = 100;
+    case 4:
+      title = tips[4].title;
+      text = tips[4].text;
       break;
     default:
-      stepProgress = 50;
+      title = tips[0].title;
+      text = tips[0].text;
   }
 
-  console.log("TITLE", displayTitle);
-  console.log("TEXT", displayText);
-
+  const getContent = () => {
+    return (
+      <Flex
+        alt="bottomContainer"
+        flexDirection="column"
+        bg="#1C1F6D"
+        height="100%"
+        borderBottomLeftRadius="10px"
+        borderBottomRightRadius="10px"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="left"
+      >
+        <Flex flexDirection="column" paddingLeft="30px" paddingRight="30px">
+          <Text fontSize="12px" color="#FF9D9D" fontWeight="700">
+            {title}
+          </Text>
+          <Text fontSize="10px" color="#FCEBEB">
+            {text}
+          </Text>
+        </Flex>
+        <ButtonGroup spacing={0} alignSelf="flex-end" paddingRight="11px">
+          <IconButton
+            aria-label="Search database"
+            icon={<ArrowLeft />}
+            onClick={() => setStep(step - 1, tips.length)}
+          />
+          <IconButton
+            aria-label="Search database"
+            icon={<ArrowRight />}
+            onClick={() => setStep(step + 1, tips.length)}
+          />
+        </ButtonGroup>
+      </Flex>
+    );
+  };
   return (
     <ChakraProvider>
       <Flex alt="container" style={containerStyle} flex={1}>
@@ -160,62 +164,52 @@ const Sleepwell = () => {
               }}
               domain={[0, 5]}
             ></YAxis>
-            <Tooltip />
+            <Tooltip
+              wrapperStyle={{
+                width: "auto",
+                height: 30,
+                background: "transparent",
+              }}
+              labelStyle={{
+                fontSize: 10,
+              }}
+              contentStyle={{
+                fontSize: 10,
+              }}
+            />
             <Legend
               layout="horizontal"
               iconType="circle"
               iconSize="5px"
-              // width={80}
               wrapperStyle={legendStyle}
             />
             <Area
-              type="monotone"
+              type="monotoneX"
+              name="Deep Sleep"
               dataKey="deepSleep"
               fill="#EEF4FF"
               stroke="#333570"
             />
-            <Line
-              type="monotone"
+            <Area
+              type="monotoneX"
+              name="Netflix after 5pm"
               dataKey="netflixHours"
+              fill="transparent"
               stroke="#FF9D9D"
               dot={false}
             />
             <Line
               type="monotone"
+              name="Optimal Sleep"
               dataKey="optimalDeepSleep"
               stroke="#333570"
+              strokeOpacity="25%"
               strokeWidth={9}
               dot={false}
             />
           </ComposedChart>
         </Flex>
-        <Flex
-          alt="bottomContainer"
-          flexDirection="column"
-          bg="#1C1F6D"
-          height="100%"
-          // paddingTop="18px"
-          borderBottomLeftRadius="10px"
-          borderBottomRightRadius="10px"
-          justifyContent="center"
-          alignItems="center"
-          textAlign="left"
-          // paddingBottom="26px"
-        >
-          <Flex flexDirection="column" paddingLeft="30px" paddingRight="36px">
-            <Text fontSize="12px" color="#FF9D9D" fontWeight="700">
-              {tips[0].title}
-            </Text>
-            <Text fontSize="10px" color="#FCEBEB">
-              {tips[0].text}
-            </Text>
-          </Flex>
-
-          <ButtonGroup spacing={0} alignSelf="flex-end" paddingRight="11px">
-            <ArrowLeft />
-            <ArrowRight />
-          </ButtonGroup>
-        </Flex>
+        {getContent()}
       </Flex>
     </ChakraProvider>
   );
