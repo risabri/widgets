2e6adb98ea1b83ea74c172588086dcb55e442520
hamza <hamzahalilovic.12@gmail.com>
2021-07-28 10:58:18 +0200

    sleepwell update

--- packages/sleepwell/stories/Sleepwell.stories.js
+++ packages/sleepwell/stories/Sleepwell.stories.js
@@ -1,9 +1,31 @@
 import React from "react";
 import Sleepwell from "../src/Sleepwell";
 
-export default { title: "Sleepwell Widget" };
+import HolisticHealth from "../../holistic-health/src/HolisticHealth";
 
-export const box = () => <Sleepwell />;
+import DryRun from "../../dry-run/src/DryRun";
+
+import FileDrop from "../../file-drop/src/Container";
+
+import {
+  Flex,
+  ChakraProvider,
+  Text,
+  IconButton,
+  ButtonGroup,
+  Stack,
+  SimpleGrid,
+} from "@chakra-ui/react";
+
+export default { title: "Widgets" };
+
+export const box = () => (
+  <SimpleGrid columns={3} spacing={5}>
+    <HolisticHealth />
+    <Sleepwell />
+    <DryRun />
+  </SimpleGrid>
+);
 box.story = {
-  name: "Sleepwell Widget",
+  name: "Widgets",
 };
